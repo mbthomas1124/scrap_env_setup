66af2cd16d75a5ddbd6d4a39f7e4b3497e48c991
Matthew Thomas <mbthomas1124@gmail.com>
2024-04-16 00:58:10 -0400

    updated scripts

--- setup.sh
+++ setup.sh
@@ -4,32 +4,32 @@ eval "$(conda shell.bash hook)"
 # directory from which the script is being run from
 CURRENT_DIR="$( pwd )"
 
-cd ~/shift-rl-sim
+cd ~/scrap_env_setup
 
 # create conda environment
 mkdir temp_download
 cd temp_download/
-conda create -n temp1 python=3.7 anaconda -y
+conda create -n rlsim python=3.7 anaconda -y
 conda clean --all -y
-conda activate temp1
+conda activate rlsim
 
 # install shift
 wget https://github.com/hanlonlab/shift-python/releases/download/v2.0.1/shift_python-2.0.1-conda_linux.zip
 unzip -j shift_python-2.0.1-conda_linux.zip
 conda install *.bz2 -y
-# conda update --all -y
 conda clean --all -y
-cd ~/shift-rl-sim
+cd ~/scrap_env_setup
 rm -rf temp_download
 
 # install pip packages
 pip install -r requirements.txt
 
-# create agents for simulations
-cd ~/shift-rl-sim/env_setup
-# ./new_agents.sh
+cd ~/scrap_env_setup
 
 # verify that python env was setup properly
 python test_env.py
+
+# modify bashrc
+echo 'conda activate rlsim"' >> ~/.bashrc
 
 cd ${CURRENT_DIR}
